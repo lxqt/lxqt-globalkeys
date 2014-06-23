559aee01dce778afc656149143ab79618a871386
Luís Pereira <luis.artur.pereira@gmail.com>
2014-06-23 13:07:32 -0700

    Adds include_directories() to the _use.cmake file
    Signed-off-by: Luís Pereira <luis.artur.pereira@gmail.com>

--- cmake/lxqt_globalkeys_ui_use.cmake
+++ cmake/lxqt_globalkeys_ui_use.cmake
@@ -1,3 +1,4 @@
+include_directories(${LXQT_GLOBALKEYS_UI_INCLUDE_DIRS})
 link_directories(${LXQT_GLOBALKEYS_UI_LIBRARY_DIRS})
 
 
--- cmake/lxqt_globalkeys_use.cmake
+++ cmake/lxqt_globalkeys_use.cmake
@@ -1,1 +1,2 @@
+include_directories(${LXQT_GLOBALKEYS_INCLUDE_DIRS})
 link_directories(${LXQT_GLOBALKEYS_LIBRARY_DIRS})
