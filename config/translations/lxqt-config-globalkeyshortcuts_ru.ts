e39c54fd3257621697c45e68be9c8ca58ba3b7e2
Ilya87 <yast4ik@yahoo.com>
2014-10-31 12:43:48 +0300

    Update Russian translation

--- config/translations/lxqt-config-globalkeyshortcuts_ru.ts
+++ config/translations/lxqt-config-globalkeyshortcuts_ru.ts
@@ -1,75 +1,168 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="ru" version="2.0">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
 <context>
-    <name>CommandFinder</name>
+    <name>DefaultModel</name>
     <message>
-        <source>...</source>
-        <translation>...</translation>
-    </message>
-    <message>
-        <source>Find a command</source>
-        <translation>Найти команду</translation>
-    </message>
-</context>
-<context>
-    <name>ShortcutConfigWindow</name>
-    <message>
-        <source>LxQt Shortcut Editor</source>
-        <translation>Редактор сочетаний клавиш LxQt</translation>
-    </message>
-    <message>
-        <source>Description</source>
-        <translation>Описание</translation>
-    </message>
-    <message>
-        <source>Shortcut</source>
-        <translation>Сочетание клавиш</translation>
-    </message>
-    <message>
+        <location filename="../default_model.cpp" line="49"/>
         <source>Command</source>
         <translation>Команда</translation>
     </message>
     <message>
-        <source>Add New</source>
-        <translation>Добавить новое</translation>
+        <location filename="../default_model.cpp" line="50"/>
+        <source>DBus call</source>
+        <translation>Вызов DBus</translation>
     </message>
     <message>
-        <source>Remove</source>
-        <translation>удалениt</translation>
+        <location filename="../default_model.cpp" line="51"/>
+        <source>Client</source>
+        <translation>Клиент</translation>
     </message>
     <message>
-        <source>Add Group</source>
-        <translation>Добавить группу</translation>
+        <location filename="../default_model.cpp" line="147"/>
+        <source>Id</source>
+        <translation>id</translation>
     </message>
     <message>
-        <source>Reset</source>
-        <translation>Сброс</translation>
+        <location filename="../default_model.cpp" line="150"/>
+        <source>Shortcut</source>
+        <translation>Сочетание клавиш</translation>
     </message>
     <message>
-        <source>Close</source>
-        <translation>Закрыть</translation>
+        <location filename="../default_model.cpp" line="153"/>
+        <source>Description</source>
+        <translation>Описание</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="156"/>
+        <source>Type</source>
+        <translation>Тип</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="159"/>
+        <source>Info</source>
+        <translation>Информация</translation>
     </message>
 </context>
 <context>
-    <name>ShortcutEditor</name>
+    <name>EditActionDialog</name>
     <message>
+        <location filename="../edit_action_dialog.ui" line="14"/>
+        <source>Edit Action</source>
+        <translation>Изменить действие</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="31"/>
+        <source>&amp;Shortcut:</source>
+        <translation>&amp;Сочетание клавиш:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="74"/>
+        <source>&amp;Description:</source>
+        <translation>&amp;Описание:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="87"/>
+        <source>&amp;Enabled</source>
+        <translation>&amp;Разрешено</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="102"/>
+        <source>&amp;Command</source>
+        <translation>&amp;Команда</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="109"/>
+        <source>&amp;DBus message</source>
+        <translation>Сообщение &amp;DBus</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="118"/>
+        <source>Type:</source>
+        <translation>Тип:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="164"/>
+        <source>Co&amp;mmand:</source>
+        <translation>Ко&amp;манда:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="206"/>
+        <source>S&amp;ervice:</source>
+        <translation>С&amp;ервис:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="219"/>
+        <source>&amp;Path:</source>
+        <translation>&amp;Путь:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="232"/>
+        <source>&amp;Interface:</source>
+        <translation>&amp;Интерфейс:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="245"/>
+        <source>&amp;Method:</source>
+        <translation>&amp;Метод:</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../main_window.ui" line="14"/>
+        <source>Global Actions Manager</source>
+        <translation>Управление глобальными действиями</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="52"/>
+        <source>Add ...</source>
+        <translation>Добавить…</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="62"/>
+        <source>Remove</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="72"/>
+        <source>Modify ...</source>
+        <translation>Изменить…</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="89"/>
+        <source>Swap</source>
+        <translation>Заменить</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="115"/>
+        <source>Multiple actions behaviour:</source>
+        <translation>Поведение нескольких действий:</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="123"/>
+        <source>First</source>
+        <translation>Первое</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="128"/>
+        <source>Last</source>
+        <translation>Последнее</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="133"/>
         <source>None</source>
         <translation>Не задано</translation>
     </message>
     <message>
-        <source>Add Shortcut</source>
-        <translation>Добавить сочетание клавиш</translation>
+        <location filename="../main_window.ui" line="138"/>
+        <source>All</source>
+        <translation>Все</translation>
     </message>
     <message>
-        <source>Remove</source>
-        <translation>удалениt</translation>
-    </message>
-    <message>
-        <source>New Group</source>
-        <translation>Новая группа</translation>
-    </message>
-    <message>
-        <source>Reset Changes</source>
-        <translation>Убрать изменения</translation>
+        <location filename="../main_window.ui" line="176"/>
+        <source>Close</source>
+        <translation>Закрыть</translation>
     </message>
 </context>
 </TS>
