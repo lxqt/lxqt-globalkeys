8682f3e8a61f61cc8f3242dfe8f5495557129d78
Mika Kobayashi <mika-k@users.noreply.github.com>
2015-01-20 15:46:40 -0500

    Update Japanese translation
    	new file:   config/translations/lxqt-config-globalkeyshortcuts_ja.desktop
    	modified:   config/translations/lxqt-config-globalkeyshortcuts_ja.ts

--- config/translations/lxqt-config-globalkeyshortcuts_ja.ts
+++ config/translations/lxqt-config-globalkeyshortcuts_ja.ts
@@ -3,46 +3,46 @@
     <name>CommandFinder</name>
     <message>
         <source>...</source>
-        <translation type="unfinished"/>
+        <translation>...</translation>
     </message>
     <message>
         <source>Find a command</source>
-        <translation type="unfinished"/>
+        <translation>コマンドを検索</translation>
     </message>
 </context>
 <context>
     <name>ShortcutConfigWindow</name>
     <message>
         <source>LxQt Shortcut Editor</source>
-        <translation type="unfinished"/>
+        <translation>LXQtショートカットエディター</translation>
     </message>
     <message>
         <source>Description</source>
-        <translation type="unfinished"/>
+        <translation>説明</translation>
     </message>
     <message>
         <source>Shortcut</source>
-        <translation type="unfinished"/>
+        <translation>ショートカット</translation>
     </message>
     <message>
         <source>Command</source>
-        <translation type="unfinished"/>
+        <translation>コマンド</translation>
     </message>
     <message>
         <source>Add New</source>
-        <translation type="unfinished"/>
+        <translation>新規追加</translation>
     </message>
     <message>
         <source>Remove</source>
-        <translation type="unfinished"/>
+        <translation>削除</translation>
     </message>
     <message>
         <source>Add Group</source>
-        <translation type="unfinished"/>
+        <translation>グループ追加</translation>
     </message>
     <message>
         <source>Reset</source>
-        <translation type="unfinished"/>
+        <translation>リセット</translation>
     </message>
     <message>
         <source>Close</source>
@@ -57,19 +57,19 @@
     </message>
     <message>
         <source>Add Shortcut</source>
-        <translation type="unfinished"/>
+        <translation>ショートカットを追加</translation>
     </message>
     <message>
         <source>Remove</source>
-        <translation type="unfinished"/>
+        <translation>削除</translation>
     </message>
     <message>
         <source>New Group</source>
-        <translation type="unfinished"/>
+        <translation>新規グループ</translation>
     </message>
     <message>
         <source>Reset Changes</source>
-        <translation type="unfinished"/>
+        <translation>変更をリセット</translation>
     </message>
 </context>
 </TS>
