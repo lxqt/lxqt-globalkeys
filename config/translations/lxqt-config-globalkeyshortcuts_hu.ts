444fda5544e996ae0a6f7bb09b819a6b19c0682c
attuska <attus@enterpriseforever.com>
2015-05-19 22:41:00 +0200

    Hungarian translation added

---
+++ config/translations/lxqt-config-globalkeyshortcuts_hu.ts
@@ -0,0 +1,93 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu_HU">
+<context>
+    <name>CommandFinder</name>
+    <message>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <source>Find a command</source>
+        <translation>Parancs keresés</translation>
+    </message>
+</context>
+<context>
+    <name>ShortcutConfigWindow</name>
+    <message>
+        <source>LxQt Shortcut Editor</source>
+        <translation>LXQt forróbillentyű szerkesztés</translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation>Leírás</translation>
+    </message>
+    <message>
+        <source>Shortcut</source>
+        <translation>Forróbillentyű</translation>
+    </message>
+    <message>
+        <source>Command</source>
+        <translation>Parancs</translation>
+    </message>
+    <message>
+        <source>Add New</source>
+        <translation>Új</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Törlés</translation>
+    </message>
+    <message>
+        <source>Add Group</source>
+        <translation>Csoport</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>Visszavon</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Bezár</translation>
+    </message>
+</context>
+<context>
+    <name>ShortcutEditor</name>
+    <message>
+        <source>None</source>
+        <translation>Nincs</translation>
+    </message>
+    <message>
+        <source>Add Shortcut</source>
+        <translation>Forróbillentyű adás</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Törlés</translation>
+    </message>
+    <message>
+        <source>New Group</source>
+        <translation>Új csoport</translation>
+    </message>
+    <message>
+        <source>Reset Changes</source>
+        <translation>Visszavonás</translation>
+    </message>
+    <message>
+        <source>Question</source>
+        <translation>Kérdés</translation>
+    </message>
+    <message>
+        <source>Delete group: %1? (everything inside will be removed altogether)</source>
+        <translation>A %1 csoport törlése? (Minden kapcsolata is törlődik)</translation>
+    </message>
+    <message>
+        <source>Delete %1?</source>
+        <translation>%1 törlése?</translation>
+    </message>
+    <message>
+        <source>Binding for %1 already exists. Replace old one?</source>
+        <translation>A %1 már foglalt. Felváltsuk az újjal?</translation>
+    </message>
+</context>
+</TS>
