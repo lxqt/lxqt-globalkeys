6bfc44f16672665db10cc7baee87e40ce2415c70
knedlyk <yupadmin@gmail.com>
2016-11-02 23:29:38 +0100

    Add/update (complete) Ukrainian translation
    * Initial translate of libfm-qt
    * libfm-qt Ukrainian translation update
    * Initial translation of compton-conf
    * compton-conf Ukrainian translation
    * Updated Ukrainian translation #1
    * Updated Ukrainian translation #2
    * Updated Ukrainian translation #3
    * Updated Ukrainian translation #4
    * Updated Ukrainian translation #5
    * Panel's plugins Ukrainian translation completed
    * Ukrainian translation update #6
    * Ukrainian translation update #7
    * Ukrainian translation update #8

--- lxqt-config-globalkeyshortcuts_uk.ts
+++ lxqt-config-globalkeyshortcuts_uk.ts
@@ -1,15 +1,15 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="uk">
+<TS version="2.0" language="uk">
 <context>
     <name>CommandFinder</name>
     <message>
         <source>...</source>
-        <translation type="vanished">...</translation>
+        <translation>...</translation>
     </message>
     <message>
         <source>Find a command</source>
-        <translation type="vanished">Знайти команду</translation>
+        <translation>Знайти команду</translation>
     </message>
 </context>
 <context>
@@ -17,42 +17,42 @@
     <message>
         <location filename="../../../default_model.cpp" line="49"/>
         <source>Command</source>
-        <translation type="unfinished">Команда</translation>
+        <translation>Команда</translation>
     </message>
     <message>
         <location filename="../../../default_model.cpp" line="50"/>
         <source>DBus call</source>
-        <translation type="unfinished"></translation>
+        <translation>Виклик DBus</translation>
     </message>
     <message>
         <location filename="../../../default_model.cpp" line="51"/>
         <source>Client</source>
-        <translation type="unfinished"></translation>
+        <translation>Клієнт</translation>
     </message>
     <message>
         <location filename="../../../default_model.cpp" line="141"/>
         <source>Id</source>
-        <translation type="unfinished"></translation>
+        <translation>Id</translation>
     </message>
     <message>
         <location filename="../../../default_model.cpp" line="144"/>
         <source>Shortcut</source>
-        <translation type="unfinished">Cкорочення</translation>
+        <translation>Cкорочення</translation>
     </message>
     <message>
         <location filename="../../../default_model.cpp" line="147"/>
         <source>Description</source>
-        <translation type="unfinished">Опис</translation>
+        <translation>Опис</translation>
     </message>
     <message>
         <location filename="../../../default_model.cpp" line="150"/>
         <source>Type</source>
-        <translation type="unfinished"></translation>
+        <translation>Тип</translation>
     </message>
     <message>
         <location filename="../../../default_model.cpp" line="153"/>
         <source>Info</source>
-        <translation type="unfinished"></translation>
+        <translation>Інформація</translation>
     </message>
 </context>
 <context>
@@ -60,62 +60,62 @@
     <message>
         <location filename="../../../edit_action_dialog.ui" line="14"/>
         <source>Edit Action</source>
-        <translation type="unfinished"></translation>
+        <translation>Редагувати дію</translation>
     </message>
     <message>
         <location filename="../../../edit_action_dialog.ui" line="31"/>
         <source>&amp;Shortcut:</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Cкорочення:</translation>
     </message>
     <message>
         <location filename="../../../edit_action_dialog.ui" line="74"/>
         <source>&amp;Description:</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Опис:</translation>
     </message>
     <message>
         <location filename="../../../edit_action_dialog.ui" line="87"/>
         <source>&amp;Enabled</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Включено</translation>
     </message>
     <message>
         <location filename="../../../edit_action_dialog.ui" line="102"/>
         <source>&amp;Command</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Команда</translation>
     </message>
     <message>
         <location filename="../../../edit_action_dialog.ui" line="109"/>
         <source>&amp;DBus message</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Повідомлення DBus</translation>
     </message>
     <message>
         <location filename="../../../edit_action_dialog.ui" line="118"/>
         <source>Type:</source>
-        <translation type="unfinished"></translation>
+        <translation>Тип:</translation>
     </message>
     <message>
         <location filename="../../../edit_action_dialog.ui" line="164"/>
         <source>Co&amp;mmand:</source>
-        <translation type="unfinished"></translation>
+        <translation>Ко&amp;манда:</translation>
     </message>
     <message>
         <location filename="../../../edit_action_dialog.ui" line="206"/>
         <source>S&amp;ervice:</source>
-        <translation type="unfinished"></translation>
+        <translation>Се&amp;рвіс:</translation>
     </message>
     <message>
         <location filename="../../../edit_action_dialog.ui" line="219"/>
         <source>&amp;Path:</source>
-        <translation type="unfinished"></translation>
+        <translation>Шля&amp;х:</translation>
     </message>
     <message>
         <location filename="../../../edit_action_dialog.ui" line="232"/>
         <source>&amp;Interface:</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Інтерфейс:</translation>
     </message>
     <message>
         <location filename="../../../edit_action_dialog.ui" line="245"/>
         <source>&amp;Method:</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Метод:</translation>
     </message>
 </context>
 <context>
@@ -123,119 +123,119 @@
     <message>
         <location filename="../../../main_window.ui" line="14"/>
         <source>Global Actions Manager</source>
-        <translation type="unfinished"></translation>
+        <translation>Управління глобальними діями</translation>
     </message>
     <message>
         <location filename="../../../main_window.ui" line="52"/>
         <source>Add ...</source>
-        <translation type="unfinished"></translation>
+        <translation>Додати ...</translation>
     </message>
     <message>
         <location filename="../../../main_window.ui" line="62"/>
         <source>Remove</source>
-        <translation type="unfinished">Вилучити</translation>
+        <translation>Вилучити</translation>
     </message>
     <message>
         <location filename="../../../main_window.ui" line="72"/>
         <source>Modify ...</source>
-        <translation type="unfinished"></translation>
+        <translation>Змінити ...</translation>
     </message>
     <message>
         <location filename="../../../main_window.ui" line="89"/>
         <source>Swap</source>
-        <translation type="unfinished"></translation>
+        <translation>Поміняти</translation>
     </message>
     <message>
         <location filename="../../../main_window.ui" line="115"/>
         <source>Multiple actions behaviour:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Поведінка багатьох дій:</translation>
     </message>
     <message>
         <location filename="../../../main_window.ui" line="123"/>
         <source>First</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Перше</translation>
     </message>
     <message>
         <location filename="../../../main_window.ui" line="128"/>
         <source>Last</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Останнє</translation>
     </message>
     <message>
         <location filename="../../../main_window.ui" line="133"/>
         <source>None</source>
-        <translation type="unfinished">Нічого</translation>
+        <translation>Нічого</translation>
     </message>
     <message>
         <location filename="../../../main_window.ui" line="138"/>
         <source>All</source>
-        <translation type="unfinished"></translation>
+        <translation>Все</translation>
     </message>
     <message>
         <location filename="../../../main_window.ui" line="176"/>
         <source>Close</source>
-        <translation type="unfinished">Закрити</translation>
+        <translation>Закрити</translation>
     </message>
 </context>
 <context>
     <name>ShortcutConfigWindow</name>
     <message>
         <source>LXQt Shortcut Editor</source>
-        <translation type="vanished">Редактор скорочень LXQt</translation>
+        <translation>Редактор скорочень LXQt</translation>
     </message>
     <message>
         <source>Description</source>
-        <translation type="vanished">Опис</translation>
+        <translation>Опис</translation>
     </message>
     <message>
         <source>Shortcut</source>
-        <translation type="vanished">Cкорочення</translation>
+        <translation>Cкорочення</translation>
     </message>
     <message>
         <source>Command</source>
-        <translation type="vanished">Команда</translation>
+        <translation>Команда</translation>
     </message>
     <message>
         <source>Add New</source>
-        <translation type="vanished">Додати нове</translation>
+        <translation>Додати нове</translation>
     </message>
     <message>
         <source>Remove</source>
-        <translation type="vanished">Вилучити</translation>
+        <translation>Вилучити</translation>
     </message>
     <message>
         <source>Add Group</source>
-        <translation type="vanished">Додати групу</translation>
+        <translation>Додати групу</translation>
     </message>
     <message>
         <source>Reset</source>
-        <translation type="vanished">Cкинути</translation>
+        <translation>Cкинути</translation>
     </message>
     <message>
         <source>Close</source>
-        <translation type="vanished">Закрити</translation>
+        <translation>Закрити</translation>
     </message>
 </context>
 <context>
     <name>ShortcutEditor</name>
     <message>
         <source>None</source>
-        <translation type="vanished">Нічого</translation>
+        <translation>Нічого</translation>
     </message>
     <message>
         <source>Add Shortcut</source>
-        <translation type="vanished">Додати скорочення</translation>
+        <translation>Додати скорочення</translation>
     </message>
     <message>
         <source>Remove</source>
-        <translation type="vanished">Вилучити</translation>
+        <translation>Вилучити</translation>
     </message>
     <message>
         <source>New Group</source>
-        <translation type="vanished">Нова група</translation>
+        <translation>Нова група</translation>
     </message>
     <message>
         <source>Reset Changes</source>
-        <translation type="vanished">Скинути зміни</translation>
+        <translation>Скинути зміни</translation>
     </message>
 </context>
 </TS>
