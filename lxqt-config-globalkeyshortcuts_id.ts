558d8ee27d1b5d0a9c05fe21f96398442c27d2e1
Arif Budiman <arifpedia@gmail.com>
2017-10-01 01:20:06 +0700

    Indonesian translation (#260)
    * Update compton-conf_id.ts
    * Create libfm-qt_id.ts
    * Added liblxqt/liblxqt_id.ts
    * Added lximage-qt/lximage-qt_id.ts
    * Added lxqt-about/lxqt-about_id.ts
    * Added lxqt-admin/lxqt-admin-time/lxqt-admin-time_id.ts
    * Added lxqt-admin/lxqt-admin-user/lxqt-admin-user_id.ts
    * Added lxqt-config/liblxqt-config-cursor/lxqt-config-cursor_id.ts
    * Added lxqt-config/lxqt-config/lxqt-config_id.ts
    * Added lxqt-config/lxqt-config-appearance/lxqt-config-appearance_id.ts
    * Added lxqt-config/lxqt-config-brightness/lxqt-config-brightness_id.ts
    * Added lxqt-config/lxqt-config-file-associations/lxqt-config-file-associations_id.ts
    * Update lxqt-config/lxqt-config-input/lxqt-config-input_id.ts
    * Added lxqt-config/lxqt-config-locale/lxqt-config-locale_id.ts
    * Added lxqt-config/lxqt-config-monitor/lxqt-config-monitor_id.ts
    * Added lxqt-config-globalkeyshortcuts_id.ts
    * Added lxqt-config-notificationd_id.ts
    * Added lxqt-notificationd_id.ts
    * Added lxqt-openssh-askpass_id.ts
    * Added lxqt-panel_id.ts
    * Added clock_id.ts
    * Added cpuload_id.ts
    * Added desktopswitch_id.ts
    * Added directorymenu_id.ts
    * Added dom_id.ts
    * Added kbindicator_id.ts
    * Added mainmenu_id.ts
    * Added mount_id.ts
    * Added networkmonitor_id.ts
    * Added quicklaunch_id.ts
    * Added sensors_id.ts
    * Added showdesktop_id.ts
    * Added spacer_id.ts
    * Added sysstat_id.ts
    * Added taskbar_id.ts
    * Added volume_id.ts
    * Added worldclock_id.ts
    * Added lxqt-policykit-agent_id.ts
    * Added lxqt-config-powermanagement_id.ts
    * Added lxqt-powermanagement_id.ts
    * Added lxqt-runner_id.ts
    * Added lxqt-config-session_id.ts
    * Added lxqt-leave_id.ts
    * Added lxqt-session_id.ts
    * Added lxqt-sudo_id.ts
    * Remove all Indonesian translation files using "id_ID"

---
+++ lxqt-config-globalkeyshortcuts_id.ts
@@ -0,0 +1,168 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="id">
+<context>
+    <name>DefaultModel</name>
+    <message>
+        <location filename="../../../default_model.cpp" line="49"/>
+        <source>Command</source>
+        <translation>Perintah</translation>
+    </message>
+    <message>
+        <location filename="../../../default_model.cpp" line="50"/>
+        <source>DBus call</source>
+        <translation>DBus call</translation>
+    </message>
+    <message>
+        <location filename="../../../default_model.cpp" line="51"/>
+        <source>Client</source>
+        <translation>Klien</translation>
+    </message>
+    <message>
+        <location filename="../../../default_model.cpp" line="141"/>
+        <source>Id</source>
+        <translation>Id</translation>
+    </message>
+    <message>
+        <location filename="../../../default_model.cpp" line="144"/>
+        <source>Shortcut</source>
+        <translation>Pintasan</translation>
+    </message>
+    <message>
+        <location filename="../../../default_model.cpp" line="147"/>
+        <source>Description</source>
+        <translation>Deskripsi</translation>
+    </message>
+    <message>
+        <location filename="../../../default_model.cpp" line="150"/>
+        <source>Type</source>
+        <translation>Tipe</translation>
+    </message>
+    <message>
+        <location filename="../../../default_model.cpp" line="153"/>
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+</context>
+<context>
+    <name>EditActionDialog</name>
+    <message>
+        <location filename="../../../edit_action_dialog.ui" line="14"/>
+        <source>Edit Action</source>
+        <translation>Sunting Tindakan</translation>
+    </message>
+    <message>
+        <location filename="../../../edit_action_dialog.ui" line="31"/>
+        <source>&amp;Shortcut:</source>
+        <translation>Pinta&amp;san:</translation>
+    </message>
+    <message>
+        <location filename="../../../edit_action_dialog.ui" line="74"/>
+        <source>&amp;Description:</source>
+        <translation>&amp;Deskripsi:</translation>
+    </message>
+    <message>
+        <location filename="../../../edit_action_dialog.ui" line="87"/>
+        <source>&amp;Enabled</source>
+        <translation>Diaktifkan</translation>
+    </message>
+    <message>
+        <location filename="../../../edit_action_dialog.ui" line="102"/>
+        <source>&amp;Command</source>
+        <translation>Perintah</translation>
+    </message>
+    <message>
+        <location filename="../../../edit_action_dialog.ui" line="109"/>
+        <source>&amp;DBus message</source>
+        <translation>Pesan &amp;DBus</translation>
+    </message>
+    <message>
+        <location filename="../../../edit_action_dialog.ui" line="118"/>
+        <source>Type:</source>
+        <translation>Tipe:</translation>
+    </message>
+    <message>
+        <location filename="../../../edit_action_dialog.ui" line="164"/>
+        <source>Co&amp;mmand:</source>
+        <translation>Perintah:</translation>
+    </message>
+    <message>
+        <location filename="../../../edit_action_dialog.ui" line="206"/>
+        <source>S&amp;ervice:</source>
+        <translation>Layanan:</translation>
+    </message>
+    <message>
+        <location filename="../../../edit_action_dialog.ui" line="219"/>
+        <source>&amp;Path:</source>
+        <translation>Jalur:</translation>
+    </message>
+    <message>
+        <location filename="../../../edit_action_dialog.ui" line="232"/>
+        <source>&amp;Interface:</source>
+        <translation>Antarmuka:</translation>
+    </message>
+    <message>
+        <location filename="../../../edit_action_dialog.ui" line="245"/>
+        <source>&amp;Method:</source>
+        <translation>&amp;Metode:</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../../main_window.ui" line="14"/>
+        <source>Global Actions Manager</source>
+        <translation>Manajer Tindakan Global</translation>
+    </message>
+    <message>
+        <location filename="../../../main_window.ui" line="52"/>
+        <source>Add ...</source>
+        <translation>Tambah ...</translation>
+    </message>
+    <message>
+        <location filename="../../../main_window.ui" line="62"/>
+        <source>Remove</source>
+        <translation>Hapus</translation>
+    </message>
+    <message>
+        <location filename="../../../main_window.ui" line="72"/>
+        <source>Modify ...</source>
+        <translation>Modif ...</translation>
+    </message>
+    <message>
+        <location filename="../../../main_window.ui" line="89"/>
+        <source>Swap</source>
+        <translation>Swap</translation>
+    </message>
+    <message>
+        <location filename="../../../main_window.ui" line="115"/>
+        <source>Multiple actions behaviour:</source>
+        <translation>Perilaku beberapa tindakan:</translation>
+    </message>
+    <message>
+        <location filename="../../../main_window.ui" line="123"/>
+        <source>First</source>
+        <translation>Pertama</translation>
+    </message>
+    <message>
+        <location filename="../../../main_window.ui" line="128"/>
+        <source>Last</source>
+        <translation>Terakhir</translation>
+    </message>
+    <message>
+        <location filename="../../../main_window.ui" line="133"/>
+        <source>None</source>
+        <translation>Tidak ada</translation>
+    </message>
+    <message>
+        <location filename="../../../main_window.ui" line="138"/>
+        <source>All</source>
+        <translation>Semua</translation>
+    </message>
+    <message>
+        <location filename="../../../main_window.ui" line="176"/>
+        <source>Close</source>
+        <translation>Tutup</translation>
+    </message>
+</context>
+</TS>
