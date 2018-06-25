47e9f93047ac84a330a1451e5ca0991dd9f96748
Yaron Shahrabani <sh.yaron@gmail.com>
2018-06-25 13:32:19 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (31 of 31 strings)
    
    Translation: LXQt/LXQt Globalkeys (Config)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-globalkeys-config/he/

--- config/translations/lxqt-config-globalkeyshortcuts_he.ts
+++ config/translations/lxqt-config-globalkeyshortcuts_he.ts
@@ -6,42 +6,42 @@
     <message>
         <location filename="../default_model.cpp" line="49"/>
         <source>Command</source>
-        <translation></translation>
+        <translation>פקודה</translation>
     </message>
     <message>
         <location filename="../default_model.cpp" line="50"/>
         <source>DBus call</source>
-        <translation></translation>
+        <translation>קריאת DBus</translation>
     </message>
     <message>
         <location filename="../default_model.cpp" line="51"/>
         <source>Client</source>
-        <translation></translation>
+        <translation>לקוח</translation>
     </message>
     <message>
         <location filename="../default_model.cpp" line="141"/>
         <source>Id</source>
-        <translation></translation>
+        <translation>מזהה</translation>
     </message>
     <message>
         <location filename="../default_model.cpp" line="144"/>
         <source>Shortcut</source>
-        <translation></translation>
+        <translation>קיצור דרך</translation>
     </message>
     <message>
         <location filename="../default_model.cpp" line="147"/>
         <source>Description</source>
-        <translation></translation>
+        <translation>תיאור</translation>
     </message>
     <message>
         <location filename="../default_model.cpp" line="150"/>
         <source>Type</source>
-        <translation></translation>
+        <translation>סוג</translation>
     </message>
     <message>
         <location filename="../default_model.cpp" line="153"/>
         <source>Info</source>
-        <translation></translation>
+        <translation>מידע</translation>
     </message>
 </context>
 <context>
@@ -49,62 +49,62 @@
     <message>
         <location filename="../edit_action_dialog.ui" line="14"/>
         <source>Edit Action</source>
-        <translation></translation>
+        <translation>עריכת פעולה</translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="31"/>
         <source>&amp;Shortcut:</source>
-        <translation></translation>
+        <translation>&amp;קיצור דרך:</translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="74"/>
         <source>&amp;Description:</source>
-        <translation></translation>
+        <translation>&amp;תיאור:</translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="87"/>
         <source>&amp;Enabled</source>
-        <translation></translation>
+        <translation>מו&amp;פעל</translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="102"/>
         <source>&amp;Command</source>
-        <translation></translation>
+        <translation>פ&amp;קודה</translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="109"/>
         <source>&amp;DBus message</source>
-        <translation></translation>
+        <translation>הו&amp;דעת DBus</translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="118"/>
         <source>Type:</source>
-        <translation></translation>
+        <translation>סוג:</translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="164"/>
         <source>Co&amp;mmand:</source>
-        <translation></translation>
+        <translation>פקו&amp;דה:</translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="206"/>
         <source>S&amp;ervice:</source>
-        <translation></translation>
+        <translation>&amp;שירות:</translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="219"/>
         <source>&amp;Path:</source>
-        <translation></translation>
+        <translation>&amp;נתיב:</translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="232"/>
         <source>&amp;Interface:</source>
-        <translation></translation>
+        <translation>&amp;מנשק:</translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="245"/>
         <source>&amp;Method:</source>
-        <translation></translation>
+        <translation>שי&amp;טה:</translation>
     </message>
 </context>
 <context>
@@ -112,57 +112,57 @@
     <message>
         <location filename="../main_window.ui" line="14"/>
         <source>Global Actions Manager</source>
-        <translation></translation>
+        <translation>מנהל פעולות גלובליות</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="52"/>
         <source>Add ...</source>
-        <translation></translation>
+        <translation>הוספה …</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="62"/>
         <source>Remove</source>
-        <translation></translation>
+        <translation>הסרה</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="72"/>
         <source>Modify ...</source>
-        <translation></translation>
+        <translation>עריכה …</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="89"/>
         <source>Swap</source>
-        <translation></translation>
+        <translation>החלפה</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="115"/>
         <source>Multiple actions behaviour:</source>
-        <translation></translation>
+        <translation>התנהגות למספר פעולות:</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="123"/>
         <source>First</source>
-        <translation></translation>
+        <translation>ראשונה</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="128"/>
         <source>Last</source>
-        <translation></translation>
+        <translation>אחרונה</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="133"/>
         <source>None</source>
-        <translation></translation>
+        <translation>ללא</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="138"/>
         <source>All</source>
-        <translation></translation>
+        <translation>הכול</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="176"/>
         <source>Close</source>
-        <translation></translation>
+        <translation>סגירה</translation>
     </message>
 </context>
 </TS>
