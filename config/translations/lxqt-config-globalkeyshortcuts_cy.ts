d574981cd4a36e9c5579fb036b8bcb1220e6ae72
Simon Quigley <tsimonq2@ubuntu.com>
2018-07-31 19:24:19 +0000

    Added translation using Weblate (Welsh)

---
+++ config/translations/lxqt-config-globalkeyshortcuts_cy.ts
@@ -0,0 +1,168 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cy">
+<context>
+    <name>DefaultModel</name>
+    <message>
+        <location filename="../default_model.cpp" line="49"/>
+        <source>Command</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="50"/>
+        <source>DBus call</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="51"/>
+        <source>Client</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="141"/>
+        <source>Id</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="144"/>
+        <source>Shortcut</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="147"/>
+        <source>Description</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="150"/>
+        <source>Type</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="153"/>
+        <source>Info</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>EditActionDialog</name>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="14"/>
+        <source>Edit Action</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="31"/>
+        <source>&amp;Shortcut:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="74"/>
+        <source>&amp;Description:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="87"/>
+        <source>&amp;Enabled</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="102"/>
+        <source>&amp;Command</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="109"/>
+        <source>&amp;DBus message</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="118"/>
+        <source>Type:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="164"/>
+        <source>Co&amp;mmand:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="206"/>
+        <source>S&amp;ervice:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="219"/>
+        <source>&amp;Path:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="232"/>
+        <source>&amp;Interface:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="245"/>
+        <source>&amp;Method:</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../main_window.ui" line="14"/>
+        <source>Global Actions Manager</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="52"/>
+        <source>Add ...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="62"/>
+        <source>Remove</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="72"/>
+        <source>Modify ...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="89"/>
+        <source>Swap</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="115"/>
+        <source>Multiple actions behaviour:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="123"/>
+        <source>First</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="128"/>
+        <source>Last</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="133"/>
+        <source>None</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="138"/>
+        <source>All</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="176"/>
+        <source>Close</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
