9ba69af82297768c1f3d80cee40c918521e1a95b
Masamichi Ito <ito32bit+github@gmail.com>
2019-09-27 22:53:15 +0000

    Translated using Weblate (Japanese)
    Currently translated at 100.0% (34 of 34 strings)
    
    Translation: LXQt/LXQt Globalkeys (Config)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-globalkeys-config/ja/

--- config/translations/lxqt-config-globalkeyshortcuts_ja.ts
+++ config/translations/lxqt-config-globalkeyshortcuts_ja.ts
@@ -137,7 +137,7 @@
     <message>
         <location filename="../main_window.ui" line="118"/>
         <source>Multiple actions behaviour:</source>
-        <translation>複数設定されたアクションの動作:</translation>
+        <translation>複数定義されたアクションの動作:</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="126"/>
@@ -162,7 +162,7 @@
     <message>
         <location filename="../main_window.ui" line="166"/>
         <source>Default</source>
-        <translation type="unfinished"></translation>
+        <translation>既定値</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="186"/>
@@ -172,13 +172,14 @@
     <message>
         <location filename="../main_window.cpp" line="187"/>
         <source>Restore Default</source>
-        <translation type="unfinished"></translation>
+        <translation>既定値に戻す</translation>
     </message>
     <message>
         <location filename="../main_window.cpp" line="188"/>
         <source>Your defined shortcuts will be removed.
 Do you want to proceed?</source>
-        <translation type="unfinished"></translation>
+        <translation>定義したショートカットを削除します。
+続けますか？</translation>
     </message>
 </context>
 </TS>
