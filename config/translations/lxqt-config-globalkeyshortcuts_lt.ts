6ed28f669b7c65cfe4553671e084b790c58f794d
Moo <hazap@hotmail.com>
2019-09-27 10:03:16 +0000

    Translated using Weblate (Lithuanian)
    Currently translated at 100.0% (34 of 34 strings)
    
    Translation: LXQt/LXQt Globalkeys (Config)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-globalkeys-config/lt/

--- config/translations/lxqt-config-globalkeyshortcuts_lt.ts
+++ config/translations/lxqt-config-globalkeyshortcuts_lt.ts
@@ -162,7 +162,7 @@
     <message>
         <location filename="../main_window.ui" line="166"/>
         <source>Default</source>
-        <translation type="unfinished"></translation>
+        <translation>Numatytasis</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="186"/>
@@ -172,13 +172,14 @@
     <message>
         <location filename="../main_window.cpp" line="187"/>
         <source>Restore Default</source>
-        <translation type="unfinished"></translation>
+        <translation>Atkurti numatytąjį</translation>
     </message>
     <message>
         <location filename="../main_window.cpp" line="188"/>
         <source>Your defined shortcuts will be removed.
 Do you want to proceed?</source>
-        <translation type="unfinished"></translation>
+        <translation>Jūsų apibrėžti spartieji klavišai bus pašalinti.
+Ar norite tęsti?</translation>
     </message>
 </context>
 </TS>
