cf4d562604754c460ba30d0268995e77d528e38a
LothaireCliquennois <cliqloth64@gnous.eu>
2019-11-11 17:19:59 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (34 of 34 strings)
    
    Translation: LXQt/LXQt Globalkeys (Config)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-globalkeys-config/fr/

--- config/translations/lxqt-config-globalkeyshortcuts_fr.ts
+++ config/translations/lxqt-config-globalkeyshortcuts_fr.ts
@@ -163,7 +163,7 @@
     <message>
         <location filename="../main_window.ui" line="166"/>
         <source>Default</source>
-        <translation type="unfinished"></translation>
+        <translation>Par défaut</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="186"/>
@@ -173,13 +173,14 @@
     <message>
         <location filename="../main_window.cpp" line="187"/>
         <source>Restore Default</source>
-        <translation type="unfinished"></translation>
+        <translation>Restaurer les paramètres par défaut</translation>
     </message>
     <message>
         <location filename="../main_window.cpp" line="188"/>
         <source>Your defined shortcuts will be removed.
 Do you want to proceed?</source>
-        <translation type="unfinished"></translation>
+        <translation>Vos raccourcis définis seront supprimés.
+Voulez-vous procéder ?</translation>
     </message>
 </context>
 </TS>
