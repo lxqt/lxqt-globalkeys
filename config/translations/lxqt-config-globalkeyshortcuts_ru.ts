c29f682f0d294b7f9afea0088b9cfb5a7bb23bd4
Golubev Alexander <fatzer2@gmail.com>
2020-01-24 13:58:38 +0000

    Translated using Weblate (Russian)
    Currently translated at 97.1% (33 of 34 strings)
    
    Translation: LXQt/LXQt Globalkeys (Config)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-globalkeys-config/ru/

--- config/translations/lxqt-config-globalkeyshortcuts_ru.ts
+++ config/translations/lxqt-config-globalkeyshortcuts_ru.ts
@@ -162,7 +162,7 @@
     <message>
         <location filename="../main_window.ui" line="166"/>
         <source>Default</source>
-        <translation type="unfinished"></translation>
+        <translation>По умолчанию</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="186"/>
@@ -172,13 +172,14 @@
     <message>
         <location filename="../main_window.cpp" line="187"/>
         <source>Restore Default</source>
-        <translation type="unfinished"></translation>
+        <translation>Восстановить значения по умолчанию</translation>
     </message>
     <message>
         <location filename="../main_window.cpp" line="188"/>
         <source>Your defined shortcuts will be removed.
 Do you want to proceed?</source>
-        <translation type="unfinished"></translation>
+        <translation>Сочетания клавиш, созданные вами будут удалены.
+Вы хотите продолжить?</translation>
     </message>
 </context>
 </TS>
