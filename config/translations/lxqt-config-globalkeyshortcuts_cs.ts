98cfdcefc3622d3ef37c53929c05ace768bc6a27
Pavel Fric <pavelfric@yahoo.com>
2020-11-06 16:06:15 +0100

    Weblate commits
    Co-authored-by: Pavel Fric <pavelfric@yahoo.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys/cs/
    Translation: LXQt Configuration/lxqt-globalkeys

--- config/translations/lxqt-config-globalkeyshortcuts_cs.ts
+++ config/translations/lxqt-config-globalkeyshortcuts_cs.ts
@@ -143,25 +143,25 @@
         <location filename="../main_window.ui" line="126"/>
         <source>First</source>
         <comment>globalkeys/config/multipleActions</comment>
-        <translation type="unfinished">První</translation>
+        <translation>První</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="131"/>
         <source>Last</source>
         <comment>globalkeys/config/multipleActions</comment>
-        <translation type="unfinished">Poslední</translation>
+        <translation>Poslední</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="136"/>
         <source>None</source>
         <comment>globalkeys/config/multipleActions</comment>
-        <translation type="unfinished">Žádná</translation>
+        <translation>Žádná</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="141"/>
         <source>All</source>
         <comment>globalkeys/config/multipleActions</comment>
-        <translation type="unfinished">Vše</translation>
+        <translation>Vše</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="166"/>
