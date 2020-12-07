13e65ff4cb00ffec7f8acb0333db6b07438ef638
Mincho Kondarev <mkondarev@yahoo.de>
2020-12-07 18:29:21 +0100

    Weblate commits
    Co-authored-by: Mincho Kondarev <mkondarev@yahoo.de>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys-menu-entry/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys/bg/
    Translation: LXQt Configuration/lxqt-globalkeys
    Translation: LXQt Configuration/lxqt-globalkeys Menu Entry

---
+++ config/translations/lxqt-config-globalkeyshortcuts_bg.ts
@@ -0,0 +1,189 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="bg">
+<context>
+    <name>DefaultModel</name>
+    <message>
+        <location filename="../default_model.cpp" line="51"/>
+        <source>Command</source>
+        <translation>Команда</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="52"/>
+        <source>DBus call</source>
+        <translation>DBus call</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="53"/>
+        <source>Client</source>
+        <translation>Клиент</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="143"/>
+        <source>Id</source>
+        <translation>Id</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="146"/>
+        <source>Shortcut</source>
+        <translation>Комбинация</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="149"/>
+        <source>Description</source>
+        <translation>Описание</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="152"/>
+        <source>Type</source>
+        <translation>Тип</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="155"/>
+        <source>Info</source>
+        <translation>Инфо</translation>
+    </message>
+</context>
+<context>
+    <name>EditActionDialog</name>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="14"/>
+        <source>Edit Action</source>
+        <translation>Редактиране</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="31"/>
+        <source>&amp;Shortcut:</source>
+        <translation>&amp;Комбинация:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="74"/>
+        <source>&amp;Description:</source>
+        <translation>&amp;Описание:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="87"/>
+        <source>&amp;Enabled</source>
+        <translation>&amp;Разрешен</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="102"/>
+        <source>&amp;Command</source>
+        <translation>&amp;Команда</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="109"/>
+        <source>&amp;DBus message</source>
+        <translation>&amp;DBus message</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="118"/>
+        <source>Type:</source>
+        <translation>Тип:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="164"/>
+        <source>Co&amp;mmand:</source>
+        <translation>К&amp;оманда:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="206"/>
+        <source>S&amp;ervice:</source>
+        <translation>&amp;Услуга:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="219"/>
+        <source>&amp;Path:</source>
+        <translation>&amp;Път:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="232"/>
+        <source>&amp;Interface:</source>
+        <translation>&amp;Интерфейс:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="245"/>
+        <source>&amp;Method:</source>
+        <translation>&amp;Метод:</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../main_window.ui" line="14"/>
+        <source>Global Actions Manager</source>
+        <translation>Конфигурация на системни клавишни комбинации</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="55"/>
+        <source>Add ...</source>
+        <translation>Добавяне...</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="65"/>
+        <source>Remove</source>
+        <translation>Премахване</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="75"/>
+        <source>Modify ...</source>
+        <translation>Промяна...</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="92"/>
+        <source>Swap</source>
+        <translation>Замени</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="118"/>
+        <source>Multiple actions behaviour:</source>
+        <translation>Поведение при множество действия:</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="126"/>
+        <source>First</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation>Начален</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="131"/>
+        <source>Last</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation>Последен</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="136"/>
+        <source>None</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation>Без</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="141"/>
+        <source>All</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation>Всички</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="166"/>
+        <source>Default</source>
+        <translation>По подразбиране</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="186"/>
+        <source>Close</source>
+        <translation>Затваряне</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="187"/>
+        <source>Restore Default</source>
+        <translation>Възстановяване на стандартни настройки</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="188"/>
+        <source>Your defined shortcuts will be removed.
+Do you want to proceed?</source>
+        <translation>Вашата зададена комбинация ще бъде премахната.
+Искате ли да продължите?</translation>
+    </message>
+</context>
+</TS>
