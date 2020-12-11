746633b59710457e98ea152b931eb0e988cfe705
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2020-12-11 18:04:00 +0100

    Translations update from Weblate (#196)
    * Weblate commits
    
    Co-authored-by: Hugo Carvalho <hugokarvalho@hotmail.com>
    
    * Weblate commits
    
    Co-authored-by: Standreas <standreas@riseup.net>
    
    * Weblate commits
    
    Co-authored-by: Mehmet Akif <madanadam@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys/tr/
    Translation: LXQt Configuration/lxqt-globalkeys
    
    Co-authored-by: Hugo Carvalho <hugokarvalho@hotmail.com>
    Co-authored-by: Standreas <standreas@riseup.net>
    Co-authored-by: Mehmet Akif <madanadam@gmail.com>

--- config/translations/lxqt-config-globalkeyshortcuts_tr.ts
+++ config/translations/lxqt-config-globalkeyshortcuts_tr.ts
@@ -143,25 +143,25 @@
         <location filename="../main_window.ui" line="126"/>
         <source>First</source>
         <comment>globalkeys/config/multipleActions</comment>
-        <translation type="unfinished">Birinci</translation>
+        <translation>İlk</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="131"/>
         <source>Last</source>
         <comment>globalkeys/config/multipleActions</comment>
-        <translation type="unfinished">Sonuncu</translation>
+        <translation>Son</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="136"/>
         <source>None</source>
         <comment>globalkeys/config/multipleActions</comment>
-        <translation type="unfinished">Hiçbiri</translation>
+        <translation>Hiçbiri</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="141"/>
         <source>All</source>
         <comment>globalkeys/config/multipleActions</comment>
-        <translation type="unfinished">Hepsi</translation>
+        <translation>Hepsi</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="166"/>
