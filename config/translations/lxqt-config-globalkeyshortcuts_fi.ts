71d802e2e9db14cbe5fca42081f35b1dfdac8639
Pyscowicz <pyskoovits@gmail.com>
2021-01-07 20:06:37 +0100

    Weblate commits
    Co-authored-by: Pyscowicz <pyskoovits@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys/fi/
    Translation: LXQt Configuration/lxqt-globalkeys

--- config/translations/lxqt-config-globalkeyshortcuts_fi.ts
+++ config/translations/lxqt-config-globalkeyshortcuts_fi.ts
@@ -143,25 +143,25 @@
         <location filename="../main_window.ui" line="126"/>
         <source>First</source>
         <comment>globalkeys/config/multipleActions</comment>
-        <translation type="unfinished">Ensimmäinen</translation>
+        <translation>Ensimmäinen</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="131"/>
         <source>Last</source>
         <comment>globalkeys/config/multipleActions</comment>
-        <translation type="unfinished">Viimeinen</translation>
+        <translation>Viimeinen</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="136"/>
         <source>None</source>
         <comment>globalkeys/config/multipleActions</comment>
-        <translation type="unfinished">Ei mitään</translation>
+        <translation>Ei mitään</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="141"/>
         <source>All</source>
         <comment>globalkeys/config/multipleActions</comment>
-        <translation type="unfinished">Kaikki</translation>
+        <translation>Kaikki</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="166"/>
