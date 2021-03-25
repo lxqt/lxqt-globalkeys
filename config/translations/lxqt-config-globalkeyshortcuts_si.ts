215e39aeab85a6981fc5b097c7f4ca2a1fb199b7
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-03-25 19:00:48 +0100

    Translations update from Weblate (#209)
    * Weblate commits
    
    Co-authored-by: helabasa <R45XvezA@pm.me>
    
    * Weblate commits
    
    Co-authored-by: Einar Mostad <einar@mostad.eu>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys/nb_NO/
    Translation: LXQt Configuration/lxqt-globalkeys
    
    Co-authored-by: helabasa <R45XvezA@pm.me>
    Co-authored-by: Einar Mostad <einar@mostad.eu>

--- config/translations/lxqt-config-globalkeyshortcuts_si.ts
+++ config/translations/lxqt-config-globalkeyshortcuts_si.ts
@@ -1,47 +1,47 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="nb_NO">
+<TS version="2.1" language="si">
 <context>
     <name>DefaultModel</name>
     <message>
         <location filename="../default_model.cpp" line="51"/>
         <source>Command</source>
-        <translation>Kommando</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../default_model.cpp" line="52"/>
         <source>DBus call</source>
-        <translation>DBus-kommando</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../default_model.cpp" line="53"/>
         <source>Client</source>
-        <translation>Klient</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../default_model.cpp" line="143"/>
         <source>Id</source>
-        <translation></translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../default_model.cpp" line="146"/>
         <source>Shortcut</source>
-        <translation>Tastatursnarevei</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../default_model.cpp" line="149"/>
         <source>Description</source>
-        <translation>Beskrivelse</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../default_model.cpp" line="152"/>
         <source>Type</source>
-        <translation></translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../default_model.cpp" line="155"/>
         <source>Info</source>
-        <translation></translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -49,62 +49,62 @@
     <message>
         <location filename="../edit_action_dialog.ui" line="14"/>
         <source>Edit Action</source>
-        <translation>Rediger handling</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="31"/>
         <source>&amp;Shortcut:</source>
-        <translation>&amp;Tastatursnarevei:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="74"/>
         <source>&amp;Description:</source>
-        <translation>&amp;Beskrivelse:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="87"/>
         <source>&amp;Enabled</source>
-        <translation>&amp;Skrudd på</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="102"/>
         <source>&amp;Command</source>
-        <translation>&amp;Kommando</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="109"/>
         <source>&amp;DBus message</source>
-        <translation>&amp;DBus-beskjed</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="118"/>
         <source>Type:</source>
-        <translation></translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="164"/>
         <source>Co&amp;mmand:</source>
-        <translation>Ko&amp;mmando:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="206"/>
         <source>S&amp;ervice:</source>
-        <translation>&amp;Tjeneste:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="219"/>
         <source>&amp;Path:</source>
-        <translation>&amp;Sti:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="232"/>
         <source>&amp;Interface:</source>
-        <translation>&amp;Grensesnitt:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="245"/>
         <source>&amp;Method:</source>
-        <translation>&amp;Metode:</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -112,78 +112,77 @@
     <message>
         <location filename="../main_window.ui" line="14"/>
         <source>Global Actions Manager</source>
-        <translation>Global handlingshåndterer</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="55"/>
         <source>Add ...</source>
-        <translation>Legg til ...</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="65"/>
         <source>Remove</source>
-        <translation>Fjern</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="75"/>
         <source>Modify ...</source>
-        <translation>Rediger ...</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="92"/>
         <source>Swap</source>
-        <translation>Bytt ut</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="118"/>
         <source>Multiple actions behaviour:</source>
-        <translation>Oppførsel ved flere handlinger:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="126"/>
         <source>First</source>
         <comment>globalkeys/config/multipleActions</comment>
-        <translation>Første</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="131"/>
         <source>Last</source>
         <comment>globalkeys/config/multipleActions</comment>
-        <translation>Siste</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="136"/>
         <source>None</source>
         <comment>globalkeys/config/multipleActions</comment>
-        <translation>Ingen</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="141"/>
         <source>All</source>
         <comment>globalkeys/config/multipleActions</comment>
-        <translation>Alle</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="166"/>
         <source>Default</source>
-        <translation>Standard</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="186"/>
         <source>Close</source>
-        <translation>Lukk</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main_window.cpp" line="187"/>
         <source>Restore Default</source>
-        <translation>Gjenopprett standardinnstillinger</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main_window.cpp" line="188"/>
         <source>Your defined shortcuts will be removed.
 Do you want to proceed?</source>
-        <translation>Dine egne tastatursnareveiertastatursnarvei blir tatt vekk.
-Ønsker du å fortsette?</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
