4aedff366dd49d9a15f0d6ad85a3c12c88f6fc66
Priit Jõerüüt <lxqtwlate@joeruut.com>
2021-04-27 22:07:16 +0200

    Weblate commits
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys-menu-entry/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys/et/
    Translation: LXQt Configuration/lxqt-globalkeys
    Translation: LXQt Configuration/lxqt-globalkeys Menu Entry

---
+++ config/translations/lxqt-config-globalkeyshortcuts_et.ts
@@ -0,0 +1,189 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="et">
+<context>
+    <name>DefaultModel</name>
+    <message>
+        <location filename="../default_model.cpp" line="51"/>
+        <source>Command</source>
+        <translation>Käsk</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="52"/>
+        <source>DBus call</source>
+        <translation>DBus-i päring</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="53"/>
+        <source>Client</source>
+        <translation>Klient</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="143"/>
+        <source>Id</source>
+        <translation>Tunnus</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="146"/>
+        <source>Shortcut</source>
+        <translation>Kiirklahv</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="149"/>
+        <source>Description</source>
+        <translation>Kirjeldus</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="152"/>
+        <source>Type</source>
+        <translation>Tüüp</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="155"/>
+        <source>Info</source>
+        <translation>Teave</translation>
+    </message>
+</context>
+<context>
+    <name>EditActionDialog</name>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="14"/>
+        <source>Edit Action</source>
+        <translation>Lisa kiirklahv või muuda kiirklahvi</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="31"/>
+        <source>&amp;Shortcut:</source>
+        <translation>&amp;Kiirklahv:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="74"/>
+        <source>&amp;Description:</source>
+        <translation>&amp;Kirjeldus:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="87"/>
+        <source>&amp;Enabled</source>
+        <translation>K&amp;asutusel</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="102"/>
+        <source>&amp;Command</source>
+        <translation>K&amp;äsk</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="109"/>
+        <source>&amp;DBus message</source>
+        <translation>&amp;DBus&apos;i sõnum</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="118"/>
+        <source>Type:</source>
+        <translation>Tüüp:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="164"/>
+        <source>Co&amp;mmand:</source>
+        <translation>K&amp;äsk:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="206"/>
+        <source>S&amp;ervice:</source>
+        <translation>T&amp;eenus:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="219"/>
+        <source>&amp;Path:</source>
+        <translation>&amp;Asukoht:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="232"/>
+        <source>&amp;Interface:</source>
+        <translation>Kasutus&amp;liides:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="245"/>
+        <source>&amp;Method:</source>
+        <translation>&amp;Meetod:</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../main_window.ui" line="14"/>
+        <source>Global Actions Manager</source>
+        <translation>Üldiste kiirklahvide haldur</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="55"/>
+        <source>Add ...</source>
+        <translation>Lisa...</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="65"/>
+        <source>Remove</source>
+        <translation>Kustuta</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="75"/>
+        <source>Modify ...</source>
+        <translation>Muuda...</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="92"/>
+        <source>Swap</source>
+        <translation>Vaheta</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="118"/>
+        <source>Multiple actions behaviour:</source>
+        <translation>Käitumine mitme toimingu puhul:</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="126"/>
+        <source>First</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation>Käivita esimene</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="131"/>
+        <source>Last</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation>Käivita viimane</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="136"/>
+        <source>None</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation>Ära tee midagi</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="141"/>
+        <source>All</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation>Käivita kõik</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="166"/>
+        <source>Default</source>
+        <translation>Taasta vaikeseadistused</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="186"/>
+        <source>Close</source>
+        <translation>Sulge</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="187"/>
+        <source>Restore Default</source>
+        <translation>Taasta vaikimisi seadistused</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="188"/>
+        <source>Your defined shortcuts will be removed.
+Do you want to proceed?</source>
+        <translation>Kustutame sinu kirjeldatud kiirklahvid.
+Kas sa soovid jätkata?</translation>
+    </message>
+</context>
+</TS>
