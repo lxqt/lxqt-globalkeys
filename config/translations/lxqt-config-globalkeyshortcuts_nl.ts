51919ea9b197a05dc04e5a490dcd0660aaf50397
Heimen Stoffels <vistausss@fastmail.com>
2021-04-27 22:07:16 +0200

    Weblate commits
    Co-authored-by: Heimen Stoffels <vistausss@fastmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys/nl/
    Translation: LXQt Configuration/lxqt-globalkeys

--- config/translations/lxqt-config-globalkeyshortcuts_nl.ts
+++ config/translations/lxqt-config-globalkeyshortcuts_nl.ts
@@ -143,25 +143,25 @@
         <location filename="../main_window.ui" line="126"/>
         <source>First</source>
         <comment>globalkeys/config/multipleActions</comment>
-        <translation type="unfinished">Eerste</translation>
+        <translation>Eerste</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="131"/>
         <source>Last</source>
         <comment>globalkeys/config/multipleActions</comment>
-        <translation type="unfinished">Laatste</translation>
+        <translation>Laatste</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="136"/>
         <source>None</source>
         <comment>globalkeys/config/multipleActions</comment>
-        <translation type="unfinished">Geen</translation>
+        <translation>Geen</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="141"/>
         <source>All</source>
         <comment>globalkeys/config/multipleActions</comment>
-        <translation type="unfinished">Alle</translation>
+        <translation>Alle</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="166"/>
