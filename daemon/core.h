0e62ba703fb02e6ff4d35f1a0df9cb7126abe912
Nils Fenner <nilsfenner@web.de>
2021-09-02 08:50:20 +0200

    inline isXYZ() key checks
    also makes those functions const

--- daemon/core.h
+++ daemon/core.h
@@ -178,9 +178,199 @@ private:
 
     QString checkShortcut(const QString &shortcut, X11Shortcut &X11shortcut);
 
-    bool isEscape(KeySym keySym, unsigned int modifiers);
-    bool isModifier(KeySym keySym);
-    bool isAllowed(KeySym keySym, unsigned int modifiers);
+    constexpr bool isEscape(KeySym keySym, unsigned int modifiers) const { return ((keySym == XK_Escape) && (!modifiers)); }
+    constexpr bool isModifier(KeySym keySym) const {
+        switch (keySym)
+        {
+        case XK_Shift_L:
+        case XK_Shift_R:
+        case XK_Control_L:
+        case XK_Control_R:
+        case XK_Meta_L:
+        case XK_Meta_R:
+        case XK_Alt_L:
+        case XK_Alt_R:
+        case XK_Super_L: // FIXME: Super_L is not a modifier (X bug?)
+        case XK_Super_R: // FIXME: Super_R is not a modifier (X bug?)
+        case XK_Hyper_L:
+        case XK_Hyper_R:
+        case XK_ISO_Level3_Shift:
+        case XK_ISO_Level5_Shift:
+        case XK_ISO_Group_Shift:
+            return true;
+
+        }
+        return false;
+    }
+    constexpr bool isAllowed(KeySym keySym, unsigned int modifiers) const {
+        switch (keySym)
+        {
+        case XK_Scroll_Lock:
+        case XK_Num_Lock:
+        case XK_Caps_Lock:
+        case XK_ISO_Lock:
+        case XK_ISO_Level3_Lock:
+        case XK_ISO_Level5_Lock:
+        case XK_ISO_Group_Lock:
+        case XK_ISO_Next_Group_Lock:
+        case XK_ISO_Prev_Group_Lock:
+        case XK_ISO_First_Group_Lock:
+        case XK_ISO_Last_Group_Lock:
+            if (!modifiers)
+            {
+                return mAllowGrabLocks;
+            }
+            break;
+
+        case XK_Home:
+        case XK_Left:
+        case XK_Up:
+        case XK_Right:
+        case XK_Down:
+        case XK_Page_Up:
+        case XK_Page_Down:
+        case XK_End:
+        case XK_Delete:
+        case XK_Insert:
+        case XK_BackSpace:
+        case XK_Tab:
+        case XK_Return:
+        case XK_space:
+            if (!modifiers)
+            {
+                return mAllowGrabBaseSpecial;
+            }
+            break;
+
+        case XK_Pause:
+        case XK_Print:
+        case XK_Linefeed:
+        case XK_Clear:
+        case XK_Multi_key:
+        case XK_Codeinput:
+        case XK_SingleCandidate:
+        case XK_MultipleCandidate:
+        case XK_PreviousCandidate:
+        case XK_Begin:
+        case XK_Select:
+        case XK_Execute:
+        case XK_Undo:
+        case XK_Redo:
+        case XK_Menu:
+        case XK_Find:
+        case XK_Cancel:
+        case XK_Help:
+        case XK_Sys_Req:
+        case XK_Break:
+            if (!modifiers)
+            {
+                return mAllowGrabMiscSpecial;
+            }
+            break;
+
+        case XK_KP_Enter:
+        case XK_KP_Home:
+        case XK_KP_Left:
+        case XK_KP_Up:
+        case XK_KP_Right:
+        case XK_KP_Down:
+        case XK_KP_Page_Up:
+        case XK_KP_Page_Down:
+        case XK_KP_End:
+        case XK_KP_Begin:
+        case XK_KP_Insert:
+        case XK_KP_Delete:
+        case XK_KP_Multiply:
+        case XK_KP_Add:
+        case XK_KP_Subtract:
+        case XK_KP_Decimal:
+        case XK_KP_Divide:
+        case XK_KP_0:
+        case XK_KP_1:
+        case XK_KP_2:
+        case XK_KP_3:
+        case XK_KP_4:
+        case XK_KP_5:
+        case XK_KP_6:
+        case XK_KP_7:
+        case XK_KP_8:
+        case XK_KP_9:
+            if (!modifiers)
+            {
+                return mAllowGrabBaseKeypad;
+            }
+            break;
+
+        case XK_KP_Space:
+        case XK_KP_Tab:
+        case XK_KP_F1:
+        case XK_KP_F2:
+        case XK_KP_F3:
+        case XK_KP_F4:
+        case XK_KP_Equal:
+        case XK_KP_Separator:
+            if (!modifiers)
+            {
+                return mAllowGrabMiscKeypad;
+            }
+            break;
+
+        case XK_grave:
+        case XK_1:
+        case XK_2:
+        case XK_3:
+        case XK_4:
+        case XK_5:
+        case XK_6:
+        case XK_7:
+        case XK_8:
+        case XK_9:
+        case XK_0:
+        case XK_minus:
+        case XK_equal:
+        case XK_Q:
+        case XK_W:
+        case XK_E:
+        case XK_R:
+        case XK_T:
+        case XK_Y:
+        case XK_U:
+        case XK_I:
+        case XK_O:
+        case XK_P:
+        case XK_bracketleft:
+        case XK_bracketright:
+        case XK_backslash:
+        case XK_A:
+        case XK_S:
+        case XK_D:
+        case XK_F:
+        case XK_G:
+        case XK_H:
+        case XK_J:
+        case XK_K:
+        case XK_L:
+        case XK_semicolon:
+        case XK_apostrophe:
+        case XK_Z:
+        case XK_X:
+        case XK_C:
+        case XK_V:
+        case XK_B:
+        case XK_N:
+        case XK_M:
+        case XK_comma:
+        case XK_period:
+        case XK_slash:
+            if (!(modifiers & ~(ShiftMask | Level3Mask | Level5Mask)))
+            {
+                return mAllowGrabPrintable;
+            }
+            break;
+
+        }
+        return true;
+    }
 
     void saveConfig();
 
