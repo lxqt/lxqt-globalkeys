f055a866348fec5e19db502b30b660c3f428e495
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-09-26 21:16:45 +0200

    Weblate commits (#235)
    Co-authored-by: 이정희 <daemul72@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/globalshortkeys-module-name/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys-menu-entry/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys/ko/
    Translation: LXQt Configuration/Globalshortcuts Module Name
    Translation: LXQt Configuration/lxqt-globalkeys
    Translation: LXQt Configuration/lxqt-globalkeys Menu Entry

---
+++ config/translations/lxqt-config-globalkeyshortcuts_ko.ts
@@ -0,0 +1,194 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko">
+<context>
+    <name>DefaultModel</name>
+    <message>
+        <location filename="../default_model.cpp" line="51"/>
+        <source>Command</source>
+        <translation>명령</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="52"/>
+        <source>DBus call</source>
+        <translation>DBus 호출</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="53"/>
+        <source>Client</source>
+        <translation>클라이언트</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="143"/>
+        <source>Id</source>
+        <translation>ID</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="146"/>
+        <source>Shortcut</source>
+        <translation>단축키</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="149"/>
+        <source>Description</source>
+        <translation>설명</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="152"/>
+        <source>Type</source>
+        <translation>유형</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="155"/>
+        <source>Info</source>
+        <translation>정보</translation>
+    </message>
+</context>
+<context>
+    <name>EditActionDialog</name>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="14"/>
+        <source>Edit Action</source>
+        <translation>작업 편집</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="31"/>
+        <source>&amp;Shortcut:</source>
+        <translation>단축키(&amp;S):</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="74"/>
+        <source>&amp;Description:</source>
+        <translation>설명(&amp;D):</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="87"/>
+        <source>&amp;Enabled</source>
+        <translation>활성화됨(&amp;E)</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="102"/>
+        <source>&amp;Command</source>
+        <translation>명령(&amp;C)</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="109"/>
+        <source>&amp;DBus message</source>
+        <translation>DBus 메시지(&amp;D)</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="118"/>
+        <source>Type:</source>
+        <translation>유형:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="164"/>
+        <source>Co&amp;mmand:</source>
+        <translation>명령(&amp;M):</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="206"/>
+        <source>S&amp;ervice:</source>
+        <translation>서비스(&amp;E):</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="219"/>
+        <source>&amp;Path:</source>
+        <translation>경로(&amp;P):</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="232"/>
+        <source>&amp;Interface:</source>
+        <translation>인터페이스(&amp;I):</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="245"/>
+        <source>&amp;Method:</source>
+        <translation>방법(&amp;M):</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../main_window.ui" line="14"/>
+        <source>Global Actions Manager</source>
+        <translation>전역 작업 관리자</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="24"/>
+        <source>Search</source>
+        <translation>검색</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="69"/>
+        <source>Add ...</source>
+        <translation>추가...</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="79"/>
+        <source>Remove</source>
+        <translation>제거</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="89"/>
+        <source>Modify ...</source>
+        <translation>수정...</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="106"/>
+        <source>Swap</source>
+        <translation>교체</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="132"/>
+        <source>Multiple actions behaviour:</source>
+        <translation>다중 작업 동작:</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="140"/>
+        <source>First</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation>첫 번째</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="145"/>
+        <source>Last</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation>마지막</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="150"/>
+        <source>None</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation>없음</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="155"/>
+        <source>All</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation>모두</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="180"/>
+        <source>Default</source>
+        <translation>기본값</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="200"/>
+        <source>Close</source>
+        <translation>닫기</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="191"/>
+        <source>Restore Default</source>
+        <translation>기본값 복원</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="192"/>
+        <source>Your defined shortcuts will be removed.
+Do you want to proceed?</source>
+        <translation>정의된 단축키가 제거됩니다.
+계속하시겠습니까?</translation>
+    </message>
+</context>
+</TS>
