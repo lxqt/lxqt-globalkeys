25288b19174be66b3c5a80e06507b026cc1db86c
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2023-01-25 15:13:48 +0800

    Weblate commits (#265)
    Co-authored-by: Quentin PAGÈS <quentinantonin@free.fr>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys/oc/
    Translation: LXQt Configuration/lxqt-globalkeys

---
+++ config/translations/lxqt-config-globalkeyshortcuts_oc.ts
@@ -0,0 +1,194 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="oc">
+<context>
+    <name>DefaultModel</name>
+    <message>
+        <location filename="../default_model.cpp" line="51"/>
+        <source>Command</source>
+        <translation>Comanda</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="52"/>
+        <source>DBus call</source>
+        <translation>Crida DBus</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="53"/>
+        <source>Client</source>
+        <translation>Client</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="143"/>
+        <source>Id</source>
+        <translation>Id</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="146"/>
+        <source>Shortcut</source>
+        <translation>Acorchi</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="149"/>
+        <source>Description</source>
+        <translation>Descripcion</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="152"/>
+        <source>Type</source>
+        <translation>Tipe</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="155"/>
+        <source>Info</source>
+        <translation>Informacions</translation>
+    </message>
+</context>
+<context>
+    <name>EditActionDialog</name>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="14"/>
+        <source>Edit Action</source>
+        <translation>Editar l’accion</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="31"/>
+        <source>&amp;Shortcut:</source>
+        <translation>&amp;Acorchi :</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="74"/>
+        <source>&amp;Description:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="87"/>
+        <source>&amp;Enabled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="102"/>
+        <source>&amp;Command</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="109"/>
+        <source>&amp;DBus message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="118"/>
+        <source>Type:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="164"/>
+        <source>Co&amp;mmand:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="206"/>
+        <source>S&amp;ervice:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="219"/>
+        <source>&amp;Path:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="232"/>
+        <source>&amp;Interface:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="245"/>
+        <source>&amp;Method:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../main_window.ui" line="14"/>
+        <source>Global Actions Manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="24"/>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="69"/>
+        <source>Add ...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="79"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="89"/>
+        <source>Modify ...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="106"/>
+        <source>Swap</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="132"/>
+        <source>Multiple actions behaviour:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="140"/>
+        <source>First</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="145"/>
+        <source>Last</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="150"/>
+        <source>None</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="155"/>
+        <source>All</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="180"/>
+        <source>Default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="200"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="191"/>
+        <source>Restore Default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="192"/>
+        <source>Your defined shortcuts will be removed.
+Do you want to proceed?</source>
+        <translation>Vòstres acorchis definits seràn suprimits.
+Volètz contunhar ?</translation>
+    </message>
+</context>
+</TS>
