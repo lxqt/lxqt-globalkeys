62c91abe8ca5acd0181553f921560f85c588052a
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2024-02-15 08:08:48 +0100

    Weblate commits (#279)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/globalshortkeys-module-name/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys-menu-entry/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys/lg/
    Translation: LXQt Configuration/lxqt-globalkeys
    Translation: LXQt Configuration/lxqt-globalkeys Menu Entry
    Translation: LXQt Configuration/lxqt-globalkeys Module Name
    
    Co-authored-by: Kizito Birabwa <kbirabwa@yahoo.co.uk>

---
+++ config/translations/lxqt-config-globalkeyshortcuts_lg.ts
@@ -0,0 +1,194 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lg">
+<context>
+    <name>DefaultModel</name>
+    <message>
+        <location filename="../default_model.cpp" line="51"/>
+        <source>Command</source>
+        <translation>Kiragiro</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="52"/>
+        <source>DBus call</source>
+        <translation>Kiragiro ekitandisi ekitambuzibwa D-BUS</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="53"/>
+        <source>Client</source>
+        <translation>Kasitoma</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="143"/>
+        <source>Id</source>
+        <translation>Endagamapeesa</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="146"/>
+        <source>Shortcut</source>
+        <translation>Mapeesa agagonza emirimu</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="149"/>
+        <source>Description</source>
+        <translation>Ekikolebwa</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="152"/>
+        <source>Type</source>
+        <translation>Kika</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="155"/>
+        <source>Info</source>
+        <translation>Ebikwata ku kikolebwa</translation>
+    </message>
+</context>
+<context>
+    <name>EditActionDialog</name>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="14"/>
+        <source>Edit Action</source>
+        <translation>Kyusamu ekikolebwa</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="31"/>
+        <source>&amp;Shortcut:</source>
+        <translation>Mapee&amp;sa agagonz&apos;emirimu:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="74"/>
+        <source>&amp;Description:</source>
+        <translation>&amp;Ekikolebwa:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="87"/>
+        <source>&amp;Enabled</source>
+        <translation>&amp;Gakola</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="102"/>
+        <source>&amp;Command</source>
+        <translation>&amp;Kiragiro</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="109"/>
+        <source>&amp;DBus message</source>
+        <translation>Bubaka obutambuzibwa &amp;DBus</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="118"/>
+        <source>Type:</source>
+        <translation>Kika:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="164"/>
+        <source>Co&amp;mmand:</source>
+        <translation>K&amp;iragiro:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="206"/>
+        <source>S&amp;ervice:</source>
+        <translation>Puloguramu &amp;empeereze:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="219"/>
+        <source>&amp;Path:</source>
+        <translation>&amp;Kkubo:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="232"/>
+        <source>&amp;Interface:</source>
+        <translation>Ens&amp;isinkano:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="245"/>
+        <source>&amp;Method:</source>
+        <translation>&amp;Mukolo:</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../main_window.ui" line="14"/>
+        <source>Global Actions Manager</source>
+        <translation>W&apos;oteekateekera amepeesa agagonz&apos;emirimu awakolerwa</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="27"/>
+        <source>Search</source>
+        <translation>Noonya</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="72"/>
+        <source>Add ...</source>
+        <translation>Yongerawo...</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="82"/>
+        <source>Remove</source>
+        <translation>Gyawo</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="92"/>
+        <source>Modify ...</source>
+        <translation>Kyusamu...</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="109"/>
+        <source>Swap</source>
+        <translation>Wanyisa</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="135"/>
+        <source>Multiple actions behaviour:</source>
+        <translation>Agategekedwa emirundi egisukka mu gumu:</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="143"/>
+        <source>First</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation>Ekisooka kye kiba kikola</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="148"/>
+        <source>Last</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation>Ekisembayo kye kiba kikola</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="153"/>
+        <source>None</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation>Galeme kukola kyonna</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="158"/>
+        <source>All</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation>Byonna ebitegekedwa bikolebwe</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="183"/>
+        <source>Default</source>
+        <translation>Zawo ebya bulijjo</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="203"/>
+        <source>Close</source>
+        <translation>Mala</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="191"/>
+        <source>Restore Default</source>
+        <translation>Zawo ebya bulijjo</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="192"/>
+        <source>Your defined shortcuts will be removed.
+Do you want to proceed?</source>
+        <translation>Bye wetegekera bijja okugyibwawo.
+Era ŋŋende mu maaso?</translation>
+    </message>
+</context>
+</TS>
