1f85f2997808b7cf571be08b5181948d0f2f38f5
Weblate <noreply@weblate.org>
2024-06-18 00:24:05 +0200

    Weblate commits
    Co-authored-by: Peter  Jespersen <flywheel@illogical.dk>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: 雪と葉 <vescrity@foxmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys-menu-entry/da/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys/da/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys/zh_CN/
    Translation: LXQt Configuration/lxqt-globalkeys
    Translation: LXQt Configuration/lxqt-globalkeys Menu Entry

--- config/translations/lxqt-config-globalkeyshortcuts_da.ts
+++ config/translations/lxqt-config-globalkeyshortcuts_da.ts
@@ -196,14 +196,16 @@ Vil du fortsætte?</translation>
     <message>
         <location filename="../main.cpp" line="51"/>
         <source>Platform unsupported</source>
-        <translation type="unfinished"></translation>
+        <translation>Platform ikke understøttet</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="52"/>
         <source>LXQt globalkeys are currently unsupported under wayland.
 
 Global shortcuts can be configured in the settings of the compositor instead.</source>
-        <translation type="unfinished"></translation>
+        <translation>LXQt globale genveje er i øjeblikket ikke understøttet under Wayland.
+
+Globale genveje skal i stedet defineres i indstillinger for kompositoren.</translation>
     </message>
 </context>
 </TS>
