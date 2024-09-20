72593ccd18644274a064197fd2e37a3174cc0f27
Weblate <noreply@weblate.org>
2024-09-20 15:24:42 +0200

    Weblate commits
    Co-authored-by: Masamichi Ito <ito32bit+github@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: 이정희 <daemul72@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys-menu-entry/ja/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys/ja/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys/ko/
    Translation: LXQt Configuration/lxqt-globalkeys
    Translation: LXQt Configuration/lxqt-globalkeys Menu Entry

--- config/translations/lxqt-config-globalkeyshortcuts_ja.ts
+++ config/translations/lxqt-config-globalkeyshortcuts_ja.ts
@@ -196,7 +196,7 @@ Do you want to proceed?</source>
     <message>
         <location filename="../main.cpp" line="51"/>
         <source>Platform Unsupported</source>
-        <translation>サポートされていないプラットフォームです</translation>
+        <translation>プラットフォームがサポートされていません</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="52"/>
