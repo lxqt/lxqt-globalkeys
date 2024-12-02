d9d1f011352ea1fb799b671856c05c6f7bf5fbcf
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2024-12-02 08:16:26 +0100

    Weblate commits (#314)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/globalshortkeys-module-name/en_GB/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/globalshortkeys-module-name/kab/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys-menu-entry/oc/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys/en_GB/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys/kab/
    Translation: LXQt Configuration/lxqt-globalkeys
    Translation: LXQt Configuration/lxqt-globalkeys Menu Entry
    Translation: LXQt Configuration/lxqt-globalkeys Module Name
    
    Co-authored-by: Andi Chandler <andi@gowling.com>
    Co-authored-by: ButterflyOfFire <butterflyoffire@protonmail.com>
    Co-authored-by: Quentin PAGÈS <quentinantonin@free.fr>

--- config/translations/lxqt-config-globalkeyshortcuts_kab.ts
+++ config/translations/lxqt-config-globalkeyshortcuts_kab.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_GB">
+<TS version="2.1" language="kab">
 <context>
     <name>DefaultModel</name>
     <message>
         <location filename="../default_model.cpp" line="51"/>
         <source>Command</source>
-        <translation>Command</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../default_model.cpp" line="52"/>
         <source>DBus call</source>
-        <translation>DBus call</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../default_model.cpp" line="53"/>
         <source>Client</source>
-        <translation>Client</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../default_model.cpp" line="143"/>
@@ -26,22 +26,22 @@
     <message>
         <location filename="../default_model.cpp" line="146"/>
         <source>Shortcut</source>
-        <translation>Shortcut</translation>
+        <translation>Anegzum</translation>
     </message>
     <message>
         <location filename="../default_model.cpp" line="149"/>
         <source>Description</source>
-        <translation>Description</translation>
+        <translation>Aglam</translation>
     </message>
     <message>
         <location filename="../default_model.cpp" line="152"/>
         <source>Type</source>
-        <translation>Type</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../default_model.cpp" line="155"/>
         <source>Info</source>
-        <translation>Info</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -49,62 +49,62 @@
     <message>
         <location filename="../edit_action_dialog.ui" line="14"/>
         <source>Edit Action</source>
-        <translation>Edit Action</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="31"/>
         <source>&amp;Shortcut:</source>
-        <translation>&amp;Shortcut:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="74"/>
         <source>&amp;Description:</source>
-        <translation>&amp;Description:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="87"/>
         <source>&amp;Enabled</source>
-        <translation>&amp;Enabled</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="102"/>
         <source>&amp;Command</source>
-        <translation>&amp;Command</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="109"/>
         <source>&amp;DBus message</source>
-        <translation>&amp;DBus message</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="118"/>
         <source>Type:</source>
-        <translation>Type:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="164"/>
         <source>Co&amp;mmand:</source>
-        <translation>Co&amp;mmand:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="206"/>
         <source>S&amp;ervice:</source>
-        <translation>S&amp;ervice:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="219"/>
         <source>&amp;Path:</source>
-        <translation>&amp;Path:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="232"/>
         <source>&amp;Interface:</source>
-        <translation>&amp;Interface:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../edit_action_dialog.ui" line="245"/>
         <source>&amp;Method:</source>
-        <translation>&amp;Method:</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -112,83 +112,82 @@
     <message>
         <location filename="../main_window.ui" line="14"/>
         <source>Global Actions Manager</source>
-        <translation>Global Actions Manager</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="27"/>
         <source>Search</source>
-        <translation>Search</translation>
+        <translation>Nadi</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="72"/>
         <source>Add ...</source>
-        <translation>Add ...</translation>
+        <translation>Rnu ...</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="82"/>
         <source>Remove</source>
-        <translation>Remove</translation>
+        <translation>Kkes</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="92"/>
         <source>Modify ...</source>
-        <translation>Modify ...</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="109"/>
         <source>Swap</source>
-        <translation>Swap</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="135"/>
         <source>Multiple actions behaviour:</source>
-        <translation>Multiple actions behaviour:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="143"/>
         <source>First</source>
         <comment>globalkeys/config/multipleActions</comment>
-        <translation>First</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="148"/>
         <source>Last</source>
         <comment>globalkeys/config/multipleActions</comment>
-        <translation>Last</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="153"/>
         <source>None</source>
         <comment>globalkeys/config/multipleActions</comment>
-        <translation>None</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="158"/>
         <source>All</source>
         <comment>globalkeys/config/multipleActions</comment>
-        <translation>All</translation>
+        <translation>Akk</translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="183"/>
         <source>Default</source>
-        <translation>Default</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main_window.ui" line="207"/>
         <source>Close</source>
-        <translation>Close</translation>
+        <translation>Mdel</translation>
     </message>
     <message>
         <location filename="../main_window.cpp" line="191"/>
         <source>Restore Default</source>
-        <translation>Restore Default</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main_window.cpp" line="192"/>
         <source>Your defined shortcuts will be removed.
 Do you want to proceed?</source>
-        <translation>Your defined shortcuts will be removed.
-Do you want to proceed?</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -196,16 +195,14 @@ Do you want to proceed?</translation>
     <message>
         <location filename="../main.cpp" line="51"/>
         <source>Platform Unsupported</source>
-        <translation>Platform Unsupported</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main.cpp" line="52"/>
         <source>LXQt globalkeys are currently unsupported under Wayland.
 
 Global shortcuts can be configured in the settings of the compositor instead.</source>
-        <translation>LXQt globalkeys are currently unsupported under Wayland.
-
-Global shortcuts can be configured in the compositor settings instead.</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
