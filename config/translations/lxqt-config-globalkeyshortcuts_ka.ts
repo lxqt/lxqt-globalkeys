9256a6077f3200fe5129d4c9b0de661797cc1177
Weblate <noreply@weblate.org>
2025-01-02 14:25:18 +0100

    Weblate commits
    Co-authored-by: NorwayFun <temuri.doghonadze@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/globalshortkeys-module-name/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys-menu-entry/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys/ka/
    Translation: LXQt Configuration/lxqt-globalkeys
    Translation: LXQt Configuration/lxqt-globalkeys Menu Entry
    Translation: LXQt Configuration/lxqt-globalkeys Module Name

---
+++ config/translations/lxqt-config-globalkeyshortcuts_ka.ts
@@ -0,0 +1,211 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ka">
+<context>
+    <name>DefaultModel</name>
+    <message>
+        <location filename="../default_model.cpp" line="51"/>
+        <source>Command</source>
+        <translation>ბრძანება</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="52"/>
+        <source>DBus call</source>
+        <translation>DBus-ის გამოძახება</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="53"/>
+        <source>Client</source>
+        <translation>კლიენტი</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="143"/>
+        <source>Id</source>
+        <translation>ID</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="146"/>
+        <source>Shortcut</source>
+        <translation>მალმხმობი</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="149"/>
+        <source>Description</source>
+        <translation>აღწერა</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="152"/>
+        <source>Type</source>
+        <translation>ტიპი</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="155"/>
+        <source>Info</source>
+        <translation>ინფორმაცია</translation>
+    </message>
+</context>
+<context>
+    <name>EditActionDialog</name>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="14"/>
+        <source>Edit Action</source>
+        <translation>ქმედების ჩასწორება</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="31"/>
+        <source>&amp;Shortcut:</source>
+        <translation>&amp;მალსახმობი:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="74"/>
+        <source>&amp;Description:</source>
+        <translation>&amp;აღწერილობა:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="87"/>
+        <source>&amp;Enabled</source>
+        <translation>&amp;ჩართულია</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="102"/>
+        <source>&amp;Command</source>
+        <translation>&amp;ბრძანება</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="109"/>
+        <source>&amp;DBus message</source>
+        <translation>&amp;DBus-ის შეტყობინება</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="118"/>
+        <source>Type:</source>
+        <translation>ტიპი:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="164"/>
+        <source>Co&amp;mmand:</source>
+        <translation>&amp;ბრძანება:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="206"/>
+        <source>S&amp;ervice:</source>
+        <translation>ს&amp;ერვისი:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="219"/>
+        <source>&amp;Path:</source>
+        <translation>&amp;ბილიკი:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="232"/>
+        <source>&amp;Interface:</source>
+        <translation>&amp;ინტერფეისი:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="245"/>
+        <source>&amp;Method:</source>
+        <translation>&amp;მეთოდი:</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../main_window.ui" line="14"/>
+        <source>Global Actions Manager</source>
+        <translation>გლობალური ქმედებების მმართველი</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="27"/>
+        <source>Search</source>
+        <translation>ძებნა</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="72"/>
+        <source>Add ...</source>
+        <translation>დამატება ...</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="82"/>
+        <source>Remove</source>
+        <translation>წაშლა</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="92"/>
+        <source>Modify ...</source>
+        <translation>შეცვლა...</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="109"/>
+        <source>Swap</source>
+        <translation>სვაპი</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="135"/>
+        <source>Multiple actions behaviour:</source>
+        <translation>ერთზე მეტი ქმედების ქევა:</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="143"/>
+        <source>First</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation>პირველი</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="148"/>
+        <source>Last</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation>ბოლო</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="153"/>
+        <source>None</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation>არაფერი</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="158"/>
+        <source>All</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation>ყველა</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="183"/>
+        <source>Default</source>
+        <translation>ნაგულისხმევი</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="207"/>
+        <source>Close</source>
+        <translation>დახურვა</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="191"/>
+        <source>Restore Default</source>
+        <translation>ნაგულისხმევის აღდგენა</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="192"/>
+        <source>Your defined shortcuts will be removed.
+Do you want to proceed?</source>
+        <translation>თქვენი აღწერილი მალსახმობები წაიშლება.
+გნებავთ, გააგრძელოთ?</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="51"/>
+        <source>Platform Unsupported</source>
+        <translation>პლატფორმა მხარდაჭერილი არაა</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="52"/>
+        <source>LXQt globalkeys are currently unsupported under Wayland.
+
+Global shortcuts can be configured in the settings of the compositor instead.</source>
+        <translation>LXQt-ის გლობალური მალსახმობები, ამჟამად, Wayland-ში მხარდაჭერილი არაა.
+
+გლობალური მალსახმობებისთვის კომპოზიტორის პარამეტრებს მიმართეთ.</translation>
+    </message>
+</context>
+</TS>
