73b5753ae278e5d8668c5c3638faf30cfaa02908
Weblate <noreply@weblate.org>
2025-04-19 11:26:02 +0200

    Weblate commits
    Co-authored-by: Pavel Fric <pavelfric@yahoo.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys/cs/
    Translation: LXQt Configuration/lxqt-globalkeys

--- config/translations/lxqt-config-globalkeyshortcuts_cs.ts
+++ config/translations/lxqt-config-globalkeyshortcuts_cs.ts
@@ -196,7 +196,7 @@ Opravdu to chcete?</translation>
     <message>
         <location filename="../main.cpp" line="51"/>
         <source>Platform Unsupported</source>
-        <translation type="unfinished"></translation>
+        <translation>Nepodporovaná platforma</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="52"/>
