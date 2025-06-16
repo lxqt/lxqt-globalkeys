1ae9e62c4582e2126c7eadbe70a829352db6491f
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2025-06-16 15:59:08 +0200

    Weblate commits (#326)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/globalshortkeys-module-name/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys-menu-entry/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-globalkeys/sv/
    Translation: LXQt Configuration/lxqt-globalkeys
    Translation: LXQt Configuration/lxqt-globalkeys Menu Entry
    Translation: LXQt Configuration/lxqt-globalkeys Module Name
    
    Co-authored-by: Simon Lindén <simon_l91@hotmail.com>

---
+++ config/translations/lxqt-config-globalkeyshortcuts_sv.ts
@@ -0,0 +1,208 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>DefaultModel</name>
+    <message>
+        <location filename="../default_model.cpp" line="51"/>
+        <source>Command</source>
+        <translation>Kommando</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="52"/>
+        <source>DBus call</source>
+        <translation>DBus-anrop</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="53"/>
+        <source>Client</source>
+        <translation>Klient</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="143"/>
+        <source>Id</source>
+        <translation>Id</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="146"/>
+        <source>Shortcut</source>
+        <translation>Tangentbordsbindning</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="149"/>
+        <source>Description</source>
+        <translation>Beskrivning</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="152"/>
+        <source>Type</source>
+        <translation>Typ</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="155"/>
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+</context>
+<context>
+    <name>EditActionDialog</name>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="14"/>
+        <source>Edit Action</source>
+        <translation>Redigera event</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="31"/>
+        <source>&amp;Shortcut:</source>
+        <translation>&amp;Bindning:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="74"/>
+        <source>&amp;Description:</source>
+        <translation>&amp;Beskrivning:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="87"/>
+        <source>&amp;Enabled</source>
+        <translation>&amp;Aktiv</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="102"/>
+        <source>&amp;Command</source>
+        <translation>&amp;Kommando</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="109"/>
+        <source>&amp;DBus message</source>
+        <translation>&amp;DBus-meddelande</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="118"/>
+        <source>Type:</source>
+        <translation>Typ:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="164"/>
+        <source>Co&amp;mmand:</source>
+        <translation>Ko&amp;mmando:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="206"/>
+        <source>S&amp;ervice:</source>
+        <translation>&amp;Tjänst:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="219"/>
+        <source>&amp;Path:</source>
+        <translation>&amp;Sökväg:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="232"/>
+        <source>&amp;Interface:</source>
+        <translation>&amp;Gränssnitt:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="245"/>
+        <source>&amp;Method:</source>
+        <translation>&amp;Metod:</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../main_window.ui" line="14"/>
+        <source>Global Actions Manager</source>
+        <translation>Hantera kortkommandon</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="27"/>
+        <source>Search</source>
+        <translation>Sök</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="72"/>
+        <source>Add ...</source>
+        <translation>Lägg till ...</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="82"/>
+        <source>Remove</source>
+        <translation>Ta bort</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="92"/>
+        <source>Modify ...</source>
+        <translation>Redigera ...</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="109"/>
+        <source>Swap</source>
+        <translation>Byt ut</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="135"/>
+        <source>Multiple actions behaviour:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="143"/>
+        <source>First</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation>Första</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="148"/>
+        <source>Last</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="153"/>
+        <source>None</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="158"/>
+        <source>All</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="183"/>
+        <source>Default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="207"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="194"/>
+        <source>Restore Default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="195"/>
+        <source>Your defined shortcuts will be removed.
+Do you want to proceed?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="51"/>
+        <source>Platform Unsupported</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="52"/>
+        <source>LXQt globalkeys are currently unsupported under Wayland.
+
+Global shortcuts can be configured in the settings of the compositor instead.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
