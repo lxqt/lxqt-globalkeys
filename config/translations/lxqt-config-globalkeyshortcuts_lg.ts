7a7a1d5fedce3db394c1e82626a8bd29a560819e
Standreas <standreas@riseup.net>
2025-10-15 21:21:55 +0200

    Updated *.ts files (#332)

--- config/translations/lxqt-config-globalkeyshortcuts_lg.ts
+++ config/translations/lxqt-config-globalkeyshortcuts_lg.ts
@@ -179,12 +179,12 @@
         <translation>Mala</translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="191"/>
+        <location filename="../main_window.cpp" line="194"/>
         <source>Restore Default</source>
         <translation>Zawo ebya bulijjo</translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="192"/>
+        <location filename="../main_window.cpp" line="195"/>
         <source>Your defined shortcuts will be removed.
 Do you want to proceed?</source>
         <translation>Bye wetegekera bijja okugyibwawo.
